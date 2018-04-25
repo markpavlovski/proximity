32649e1888b1260068be2ff89a9276829e2e496b
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-25 13:33:45 -0700

    added distance-specific message query

--- src/app.js
+++ src/app.js
@@ -1,22 +1,35 @@
 (function() {
   'use strict';
 
-  
+  const distanceEl = document.querySelector('#distance-range')
+  console.log(distanceEl.value);
 
 
-  // authe gate
-  request('/auth/token')
-  .then(function(response){
-    return response
-  })
-  .then(response=>{
-    console.log(`user ${response.data.id} is logged in`)
-    return request(`/messages/10000`)
-  })
-  .then(messages => console.log(messages.data.data.rows))
-  .catch(function(error){
-    console.log('hiiii');
+  getMessages(distanceEl.value)
+
+
+  distanceEl.addEventListener('mouseup',event=>{
+    console.log(distanceEl.value);
+    getMessages(distanceEl.value)
   })
 
 
 })();
+
+
+function getMessages(distance,friendsOnly = false){
+
+    // authe gate
+    request('/auth/token')
+    .then(function(response){
+      return response
+    })
+    .then(response=>{
+      console.log(`user ${response.data.id} is logged in`)
+      return request(`/messages/${distance}`)
+    })
+    .then(messages => console.log(messages.data.data.rows))
+    .catch(function(error){
+      console.log('hiiii');
+    })
+}
